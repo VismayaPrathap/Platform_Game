0816225743df23a5de54fa807d5add1ea73f294e
Vismaya Prathap <85412786+VismayaPrathap@users.noreply.github.com>
2023-05-24 10:24:49 +0530

    Add files via upload

--- levelmap.py
+++ levelmap.py
@@ -6,14 +6,14 @@ level_map = [
     '       xx   xx              ',
     '                    xxx     ',
     'xxx           xxxx          ',
-    'xxxxx                       ',
+    'xxxxx P                      ',
     'xxxxxxx   x  xxxx     xxxx  ',
     'xxxxx     x  xxxx    xxxxx  ',
     'xxx       x  xxxxxx  xxxxx  ',
     '       xxxx  xxxxxx  xxxxx  ',
     '     xxxxxx  xxxxxx  xxxxx  ',]
 
-tileSize=35
-sWidth=900
-sHeight= len(level_map)* tileSize
+tileSize = 35
+sWidth = 900
+sHeight = len(level_map) * tileSize
 print(sHeight)
